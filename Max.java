621330dfd833d4b82977307f8a5470f2778cfad4
KazHiryu187 <47704184+kazhiryu187@users.noreply.github.com>
2020-04-11 17:04:57 -0400

    small update on Max

--- Max.java
+++ Max.java
@@ -3,12 +3,12 @@ public class Max{
 	public static void main(String[] args){
 		
 		System.out.println("----------------------------------");
-		System.out.println(Byte.MAX_VALUE);
-		System.out.println(Short.MAX_VALUE);
-		System.out.println(Integer.MAX_VALUE);
-		System.out.println(Long.MAX_VALUE);
-		System.out.println(Float.MAX_VALUE);
-		System.out.println(Double.MAX_VALUE);
+		System.out.println("Byte = " + Byte.MAX_VALUE);
+		System.out.println("Short = " + Short.MAX_VALUE);
+		System.out.println("Integer = " + Integer.MAX_VALUE);
+		System.out.println("Long = " + Long.MAX_VALUE);
+		System.out.println("Float = " + Float.MAX_VALUE);
+		System.out.println("Double = " + Double.MAX_VALUE);
 		System.out.println("----------------------------------");
 	}
 }
