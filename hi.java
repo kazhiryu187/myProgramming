24c56b67ba789ca865a7a9a57281ed4e27b76843
KazHiryu187 <47704184+kazhiryu187@users.noreply.github.com>
2020-04-13 10:39:01 -0400

    Add files via upload

--- hi.java
+++ hi.java
@@ -1,11 +1,27 @@
 public class hi {
+	
+	//This how to comment, single line anyway
+	
+	static String a1 ="PGx632&opG!@#azu";
     
 	public static void main(String[] args){
-	 
+		
+		short v1 = 237; //changed to short from int, 4-6-20
+		short v2 = 356;
+		int v3 = v1 + v2;
+		int v4 = v3 * v2;
+		
+		boolean b1 = true;
+		
+	 System.out.println(" ");
 	 System.out.println("...//!!\\...");
 	 System.out.println("-=QW4@um007=-");
 	 System.out.println("------------");
 	 System.out.println("Dirt9tRee9M@N");
-	
+	 System.out.println(v3);
+	 System.out.println(v4);
+	 System.out.println(a1);
+	 System.out.println(b1);
+	 System.out.println(Byte.MAX_VALUE);
 	}
 	  }
---
+++ russia.java
@@ -0,0 +1,35 @@
+public class russia{
+	
+	static final double d3 = 3.14;
+	
+	public static void main(String[] args){
+		
+		int n1 = 7546;
+		int n2 = 2435;
+		double d1 = 223.4;
+		double d2 = n1 + d1; //can use ops on a double and intger, sum must be double
+		double d4 = d1 + d2 + d3;
+		int n3 = n1 * n2;
+		int n4 = n3 + n1;
+		
+		System.out.println("=============");
+		System.out.println(" ");
+		System.out.println(d2);
+		System.out.println(d3);
+		System.out.println(d4);
+		System.out.println(" ");
+		System.out.println("=============");
+		System.out.println(" ");
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(d1);
+		System.out.println(n3 + " " + n4);
+		System.out.println(" ");
+		System.out.println("=============");
+		System.out.println(" ");
+		System.out.println(n3);
+		System.out.println(n4);
+		System.out.println(" ");
+		System.out.println("=============");
+	} 
+}
