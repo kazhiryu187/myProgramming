400a7ece6637da4c3cd4872f28ec00025a13b77e
kazhiryu187 <kazhiryu187@gmail.com>
2020-09-05 08:43:49 -0400

    blah

--- RNG.py
+++ RNG.py
@@ -1,4 +1,7 @@
 import random
-print (random.randrange(999))
-print (random.randrange(1, 15))
-print (random.randrange(4, 11, 3))
+# print (random.randrange(999))
+# print (random.randrange(1, 15))
+# print (random.randrange(4, 11, 3))
+
+for i in range(10): 
+  print (random.random())
