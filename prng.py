92abd519c3c255e9309a4670ccfe0279bb3af479
kazhiryu187 <kazhiryu187@gmail.com>
2020-09-11 05:52:34 -0400

    test random in var

--- prng.py
+++ prng.py
@@ -1,8 +1,12 @@
 import random
 
+va = random.randrange(0,80)
+
 print (random.randrange(0,80))
 print (random.randrange(0,80))
 print (random.randrange(0,80))
 print (random.randrange(0,80))
 print (random.randrange(0,80))
 print (random.randrange(0,80))
+print (" ")
+print (va)
