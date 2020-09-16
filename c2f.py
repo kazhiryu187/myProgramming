486245bea1689ced09dec11714173af5f49098f9
kazhiryu187 <kazhiryu187@gmail.com>
2020-09-16 08:00:47 -0400

    simple celsius converter

---
+++ c2f.py
@@ -0,0 +1,9 @@
+# Celsius to Fahrenheit converter
+
+cel = int(input('Temperature in Celsius: '))
+
+#cel = int(cel)
+
+fah = (cel * 9/5) + 32
+
+print("{} Celsius = {} Fahrenheit ".format(cel, fah))
